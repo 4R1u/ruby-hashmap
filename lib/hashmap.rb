e827533e76957cc6e58d6d3ee116d3a075f816f5
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-10 12:02:10 +0400

    Implement Basic HashMap

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,1 +1,9 @@
 # frozen_string_literal: true
+
+# Hashmap Implementation
+class Hashmap
+  def initialize(capacity = 16, load_factor = 0.875)
+    @capacity = capacity
+    @load_factor = load_factor
+  end
+end
