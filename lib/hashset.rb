8caea272e547489b668cf64b1bc081e0edf57a42
4R1u <62248461+4R1u@users.noreply.github.com>
2024-12-10 17:51:22 +0400

    Rename hashset's Hashmap to HashSet

--- lib/hashset.rb
+++ lib/hashset.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-# Hashmap Implementation
-class HashMap
+# Set Implementation, Like a Hashmap But with no Values
+class HashSet
   attr_reader :length
 
   def initialize(capacity = 16, load_factor = 0.875)
